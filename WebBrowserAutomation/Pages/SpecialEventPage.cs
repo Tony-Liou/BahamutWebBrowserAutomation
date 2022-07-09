95784e1db9cf7970985708f65f86f63f32f67ac4
Tony Liou <Tony-Liou@users.noreply.github.com>
2022-07-09 14:44:42 +0800

    Add some config files and a developing page

---
+++ WebBrowserAutomation/Pages/SpecialEventPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace WebBrowserAutomation.Pages;
+
+// TODO: This page is for Home page if there is a event page before entering the home page.
+class SpecialEventPage
+{
+    private readonly WebDriver _driver;
+    
+    public SpecialEventPage(WebDriver driver)
+    {
+        _driver = driver;
+    }
+}
